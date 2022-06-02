d182005de6f507886a0b2e3bf0cd48cb2993a7f7
Jonathan Adiwibowo <jonathan.adiwibowo@dropezy.com>
2022-06-02 18:14:10 +0700

    chore: Update dummy for product category

--- lib/features/product_search/domain/services/dummy_product_inventory_repository.dart
+++ lib/features/product_search/domain/services/dummy_product_inventory_repository.dart
@@ -1,60 +1,115 @@
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 import 'package:storefront_app/core/models/repo_result.dart';
-import 'package:storefront_app/features/product/domain/models/product_model.dart';
 
 import '../../../../di/di_environment.dart';
+import '../../../product/index.dart';
 import '../../index.dart';
 
 @LazySingleton(as: IProductInventoryRepository, env: [DiEnvironment.dummy])
 class DummyProductInventoryRepository extends IProductInventoryRepository {
-  static const productSeladaRomaine = ProductModel(
-    productId: 'selada-romaine-id',
-    sku: 'selada-romaine-sku',
-    name: 'Selada Romaine',
-    price: '15000',
-    stock: 2,
-    variants: [],
-    defaultProduct: '',
-    imagesUrls: [''],
-    unit: '500g',
-    description: dummyDescription,
-    thumbnailUrl: 'https://pngimg.com/uploads/spinach/spinach_PNG65.png',
+  static const productTehBotol = ProductModel(
+    productId: 'teh-botol-sosro-original',
+    sku: 'SKUTBS002',
+    name: 'Teh Botol Sosro Original',
+    price: '3200',
+    stock: 50,
+    variants: [
+      VariantModel(
+        variantId: '0-variant-id',
+        name: '250ml / Pcs',
+        defaultImageUrl: 'https://i.imgur.com/rHfndKT.jpeg',
+        imagesUrls: [
+          'https://i.imgur.com/rHfndKT.jpeg',
+        ],
+        price: '3200',
+        sku: 'SKUTBS002',
+        stock: 15,
+        unit: '250ml / Pcs',
+      ),
+      VariantModel(
+        variantId: '1-variant-id',
+        name: '330ml / Pcs',
+        defaultImageUrl: 'https://i.imgur.com/JxNPDNU.jpeg',
+        imagesUrls: [
+          'https://i.imgur.com/JxNPDNU.jpeg',
+        ],
+        price: '3500',
+        sku: 'SKUTBS003',
+        stock: 15,
+        unit: '330ml / Pcs',
+      ),
+      VariantModel(
+        variantId: '2-variant-id',
+        name: '450ml / Botol',
+        defaultImageUrl: 'https://i.imgur.com/3hGn49J.jpeg',
+        imagesUrls: [
+          'https://i.imgur.com/3hGn49J.jpeg',
+        ],
+        price: '6000',
+        sku: 'SKUTBS001',
+        stock: 15,
+        unit: '450ml / Botol',
+      ),
+    ],
+    defaultProduct: '0-variant-id',
+    unit: '250ml / Pcs',
+    description:
+        'Teh Botol Sosro Original Kotak dihasilkan dari daun teh dengan kualitas terbaik dengan cita rasa dan aroma jasmine yang khas',
+    thumbnailUrl: 'https://i.imgur.com/rHfndKT.jpeg',
+    imagesUrls: [
+      'https://i.imgur.com/rHfndKT.jpeg',
+      'https://i.imgur.com/JxNPDNU.jpeg',
+      'https://i.imgur.com/3hGn49J.jpeg'
+    ],
   );
 
-  static const productSeledri = ProductModel(
-    productId: 'seledri-id',
-    sku: 'seledri-sku',
-    name: 'Seledri',
-    price: '150000',
-    discount: '200000',
-    stock: 20,
-    variants: [],
-    defaultProduct: '',
-    imagesUrls: [''],
-    unit: '500g',
-    description: dummyDescription,
-    thumbnailUrl: 'https://pngimg.com/uploads/celery/celery_PNG12.png',
+  static const productKacangHijau = ProductModel(
+    productId: 'ultra-milk-sari-kacang-hijau',
+    sku: 'SKU0360',
+    name: 'Ultra Milk Sari Kacang Hijau',
+    price: '3900',
+    stock: 50,
+    variants: [
+      VariantModel(
+        variantId: '0-variant-id',
+        name: '250ml / Pcs',
+        defaultImageUrl: 'https://i.imgur.com/h31ZPb2.jpeg',
+        imagesUrls: [
+          'https://i.imgur.com/h31ZPb2.jpeg',
+        ],
+        price: '3900',
+        sku: 'SKU0360',
+        stock: 15,
+        unit: '250ml / Pcs',
+      ),
+    ],
+    defaultProduct: '0-variant-id',
+    unit: '250ml / Pcs',
+    description:
+        'Ultra sari kacang hijau minuman sari kacang hijau siap minum.',
+    thumbnailUrl: 'https://i.imgur.com/h31ZPb2.jpeg',
+    imagesUrls: ['https://i.imgur.com/h31ZPb2.jpeg'],
   );
 
   final productsCategoryList = [
     // Sayuran Batang
-    productSeledri.copyWith(id: '4'),
-    productSeledri.copyWith(id: '4'),
+    productKacangHijau.copyWith(id: '4'),
+    productKacangHijau.copyWith(id: '4'),
     //Sayuran Bunga
-    productSeledri.copyWith(id: '1'),
-    productSeledri.copyWith(id: '1'),
-    productSeladaRomaine.copyWith(id: '1'),
-    productSeladaRomaine.copyWith(id: '1'),
+    productKacangHijau.copyWith(id: '1'),
+    productKacangHijau.copyWith(id: '1'),
+    productTehBotol.copyWith(id: '1'),
+    productTehBotol.copyWith(id: '1'),
     //Sayuran Bawang
-    productSeladaRomaine.copyWith(id: '5'),
-    productSeladaRomaine.copyWith(id: '5'),
-    productSeledri.copyWith(id: '5'),
-    productSeledri.copyWith(id: '5'),
-    productSeladaRomaine.copyWith(id: '5'),
-    productSeladaRomaine.copyWith(id: '5'),
-    productSeledri.copyWith(id: '5'),
-    productSeledri.copyWith(id: '5'),
+    productTehBotol.copyWith(id: '5'),
+    productTehBotol.copyWith(id: '5'),
+    productKacangHijau.copyWith(id: '5'),
+    productKacangHijau.copyWith(id: '5'),
+    productTehBotol.copyWith(id: '5'),
+    productTehBotol.copyWith(id: '5'),
+    productKacangHijau.copyWith(id: '5'),
+    productKacangHijau.copyWith(id: '5'),
   ];
 
   @override
