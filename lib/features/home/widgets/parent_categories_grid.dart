fb37d1adbcf97422688765a3a643508dadfe7f12
Jonathan Adiwibowo <jonathan.adiwibowo@dropezy.com>
2022-06-09 19:36:57 +0700

    Fix: parent categories grid minimum height

--- lib/features/home/widgets/parent_categories_grid.dart
+++ lib/features/home/widgets/parent_categories_grid.dart
@@ -12,63 +12,81 @@ class ParentCategoriesGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<ParentCategoriesCubit, ParentCategoriesState>(
       builder: (context, state) {
+        // TODO (Jonathan) : try to find better solving
+        // Iphone 13 screen height is 667
+        final screenSize = MediaQuery.of(context).size.height;
+
+        final appBarHeight = context.res.dimens.appBarSize;
+
+        // Navbar height is 56
+        final navBarHeight = screenSize * 0.08;
+
+        // Search Area height is 180
+        final searchHeight = screenSize * 0.28;
+
+        final parentCategoriesHeight =
+            screenSize - (appBarHeight + navBarHeight + searchHeight);
+
         if (state is LoadingParentCategoriesState) {
           return SizedBox(
             // TODO (Jonathan) : implements skeleton
-            height: MediaQuery.of(context).size.height * .40,
+            height: parentCategoriesHeight,
             child: const Center(
               key: ValueKey(HomePageKeys.loadingparentCategoryWidget),
               child: CircularProgressIndicator(),
             ),
           );
         } else if (state is LoadedParentCategoriesState) {
-          return GridView.builder(
-            shrinkWrap: true,
-            key: const ValueKey(HomePageKeys.parentCategoryGridWidget),
-            physics: const NeverScrollableScrollPhysics(),
-            //TODO (Jonathan): update into dynamic
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 100,
-              childAspectRatio: 3 / 5,
-              // mainAxisExtent: 140,
-              crossAxisSpacing: 16,
-              mainAxisSpacing: 12,
-            ),
-            padding: EdgeInsets.zero,
-            itemCount: state.parentCategoryList.length,
-            itemBuilder: (context, index) {
-              return GridTile(
-                child: GestureDetector(
-                  onTap: () {
-                    context.router.push(
-                      ChildCategoriesRoute(
-                        parentCategoryModel: state.parentCategoryList[index],
-                      ),
-                    );
-                  },
-                  child: Column(
-                    children: <Widget>[
-                      Flexible(
-                        flex: 3,
-                        child: DropezyImage(
-                          url: state.parentCategoryList[index].thumbnailUrl,
-                          borderRadius: 16,
+          return SizedBox(
+            height: parentCategoriesHeight,
+            child: GridView.builder(
+              shrinkWrap: true,
+              key: const ValueKey(HomePageKeys.parentCategoryGridWidget),
+              physics: const NeverScrollableScrollPhysics(),
+              //TODO (Jonathan): update into dynamic
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 100,
+                childAspectRatio: 3 / 5,
+                // mainAxisExtent: 140,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 12,
+              ),
+              padding: EdgeInsets.zero,
+              itemCount: state.parentCategoryList.length,
+              itemBuilder: (context, index) {
+                return GridTile(
+                  child: GestureDetector(
+                    onTap: () {
+                      context.router.push(
+                        ChildCategoriesRoute(
+                          parentCategoryModel: state.parentCategoryList[index],
                         ),
-                      ),
-                      const SizedBox(height: 4),
-                      Flexible(
-                        flex: 2,
-                        child: Text(
-                          state.parentCategoryList[index].name,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(fontSize: 13),
+                      );
+                    },
+                    child: Column(
+                      children: <Widget>[
+                        Flexible(
+                          flex: 3,
+                          child: DropezyImage(
+                            url: state.parentCategoryList[index].thumbnailUrl,
+                            borderRadius: 16,
+                          ),
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 4),
+                        Flexible(
+                          flex: 2,
+                          child: Text(
+                            state.parentCategoryList[index].name,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(fontSize: 13),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           );
         } else if (state is ErrorLoadingParentCategoriesState) {
           return Center(
