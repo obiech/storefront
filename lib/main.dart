f15497bd89dc354de96825218f988f090ff2c895
Leonardo Vinsen <leonardo@dropezy.com>
2022-06-12 19:34:23 +0700

    feat: enable Firebase App Check
    [STOR-121]

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
@@ -27,9 +28,13 @@ Future<void> main() async {
       WidgetsFlutterBinding.ensureInitialized();
 
       await dotenv.load(fileName: 'env/.env');
+
+      // Initialize Firebase depencencies
+      // AppCheck must come after Firebase.initializeApp
       await Firebase.initializeApp();
       FlutterError.onError =
           FirebaseCrashlytics.instance.recordFlutterFatalError;
+      await FirebaseAppCheck.instance.activate();
 
       await Hive.initFlutter();
 
